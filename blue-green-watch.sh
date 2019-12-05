3a38455c935b68953b1e7977e9e4c0e0ceeda0ff
Ubuntu <jenkins@jenkinsVM.1aq4hkw24tgenmg2zqbtjklmgg.fx.internal.cloudapp.net>
2019-12-05 10:12:45 +0000

    .

--- blue-green-watch.sh
+++ blue-green-watch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # Requires 'helm', 'yq', 'jq', 'sed', 'curl'
 slots=( "blue" "green" ) apis=( "api-poi" "api-trip" "api-user" "api-user-java" ) format="%-15s %-5s %-10s %-6s %-12s %-8s %s"
-
 declare -A helmValues
 function downloadHelmData { helm get values --all "$1" | yq . ; }
 for api in "${apis[@]}"; do
     helmValues["${api}"]="$(downloadHelmData "${api}")"
 done
-function getHelmData { echo "${helmValues["$1"]}" ; }
-
+function getCachedHelmData { echo "${helmValues["$1"]}" ; }
 function getJsonVal { echo $(echo "$1" | jq -r "$2") ; }
 function prodSlot { echo $(getJsonVal "$1" ".productionSlot") ; }
 function status { echo $(getJsonVal "$1" ".$2.enabled" | sed 's/true/enabled/g' | sed 's/false/disabled/g') ; }
@@ -19,9 +17,13 @@ function healthURL { echo "$(healthHost "$1")$(healthPath "$1")" ; }
 function http_status { echo $(curl --silent --output /dev/null --write-out '%{http_code}' $1) ; }
 function displayProd { if [ $1 == $2 ]; then echo "Production"; else echo "Staging"; fi ; }
 function displayHealth { if [ $1 == $2 ]; then echo "$3"; else echo "$4"; fi ; }
-function displayHeader { printf "${format}\n" "api" "slot" "role" "health" "status" "tag"; }
+function displayHeader { printf "${format}\n" "api" "slot" "role" "health" "status" "tag" ; }
 function displaySlot {
-    api="$1" slot="$2" json=$(getHelmData ${api}) productionSlot=$(prodSlot "${json}") healthProd=$(http_status "http://$(healthURL "${json}")") healthStaging=$(http_status "http://stage$(healthURL "${json}")")
+    api="$1" slot="$2"
+    json=$(getCachedHelmData ${api})
+    productionSlot=$(prodSlot "${json}")
+    healthProd=$(http_status "http://$(healthURL "${json}")")
+    healthStaging=$(http_status "http://stage$(healthURL "${json}")")
     printf "${format}" "${api}" "${slot}" \
         "$(displayProd "${slot}" "${productionSlot}")" \
         "$(displayHealth "${slot}" "${productionSlot}" "${healthProd}" "${healthStaging}")" \
@@ -30,8 +32,8 @@ function displaySlot {
 }
 
 echo "$(displayHeader)"
-for slot in "${slots[@]}"; do 
 for api in "${apis[@]}"; do 
+for slot in "${slots[@]}"; do 
 echo "$(displaySlot "${api}" "${slot}")"
 done
 done
