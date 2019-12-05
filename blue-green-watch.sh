2348d8fc8579933c4dbaaf61dbad92782812f2cb
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-12-05 11:58:35 +0000

    name fixes

--- blue-green-watch.sh
+++ blue-green-watch.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 # Requires 'helm', 'yq', 'jq', 'sed', 'curl', 'printf"
 # Author: Christian Geuer-Pollmann <chgeuer@microsoft.com>
-slots=( "blue" "green" ) apis=( "api-poi" "api-trip" "api-user" "api-user-java" ) format="| %-15s | %-5s | %-10s | %-6s | %-12s | %-8s | %s"
+declare slots=( "blue" "green" ) releaseNames=( "api-poi" "api-trip" "api-user" "api-user-java" ) format="| %-15s | %-5s | %-10s | %-6s | %-12s | %-8s | %s"
 declare -A helmValues
-function downloadHelmData { helm get values --all "$1" | yq . ; }
-for api in "${apis[@]}"; do
-    helmValues["${api}"]="$(downloadHelmData "${api}")"
+function downloadHelmData { local releaseName="$1"; helm get values --all "${releaseName}" | yq . ; }
+for releaseName in "${releaseNames[@]}"; do
+    helmValues["${releaseName}"]="$(downloadHelmData "${releaseName}")"
 done
-function getCachedHelmData { echo "${helmValues["$1"]}" ; }
+function getCachedHelmData { local releaseName="$1" ; echo "${helmValues["${releaseName}"]}" ; }
 function getJsonVal { echo $(echo "$1" | jq -r "$2") ; }
 function prodSlot { echo $(getJsonVal "$1" ".productionSlot") ; }
-function status { echo $(getJsonVal "$1" ".$2.enabled" | sed 's/true/enabled/g' | sed 's/false/disabled/g') ; }
-function tag { echo $(getJsonVal "$1" ".$2.tag") ; }
+function status { echo $(getJsonVal "$1" ".$2.enabled" | sed 's/true/running/g' | sed 's/false/empty/g') ; }
+function tag { local json="$1" slot="$2"; echo $(getJsonVal "${json}" ".${slot}.tag") ; }
 function healthHost { echo "$(getJsonVal "$1" .ingress.rules.endpoint.host)" ; }
 function healthPath { echo "$(getJsonVal "$1" ".ingress.rules.endpoint.paths[] | select(.path | contains(\"healthcheck\")) | .path")" ; }
 function healthURL { echo "$(healthHost "$1")$(healthPath "$1")" ; }
 function http_status { echo $(curl --silent --output /dev/null --write-out '%{http_code}' $1) ; }
-function displayProd { if [ $1 == $2 ]; then echo "Production"; else echo "Staging"; fi ; }
+function displayProd { local s1="$1" s2="$2"; if [ "${s1}" == "${s2}" ]; then echo "Production"; else echo "Staging"; fi ; }
 function displayHealth { if [ $1 == $2 ]; then echo "$3"; else echo "$4"; fi ; }
 function displayHeader { printf "${format}\n" "helm release" "slot" "role" "health" "status" "tag" ; }
 function displaySep { printf "${format}\n" "---------------" "-----" "----------" "------" "------------" "--------" ; }
 function displaySlot {
-    api="$1" slot="$2"
-    json=$(getCachedHelmData ${api})
-    productionSlot=$(prodSlot "${json}")
-    healthProd=$(http_status "http://$(healthURL "${json}")")
-    healthStaging=$(http_status "http://stage$(healthURL "${json}")")
-    printf "${format}" "${api}" "${slot}" \
+    local releaseName="$1" slot="$2" json=$(getCachedHelmData ${releaseName})
+    local productionSlot=$(prodSlot "${json}")
+    local healthProd=$(http_status "http://$(healthURL "${json}")")
+    local healthStaging=$(http_status "http://stage$(healthURL "${json}")")
+    printf "${format}" "${releaseName}" "${slot}" \
         "$(displayProd "${slot}" "${productionSlot}")" \
         "$(displayHealth "${slot}" "${productionSlot}" "${healthProd}" "${healthStaging}")" \
         "$(status "${json}" "${slot}")" \
@@ -36,7 +35,7 @@ function displaySlot {
 echo "$(displayHeader)"
 for slot in "${slots[@]}"; do 
 echo "$(displaySep)"
-for api in "${apis[@]}"; do 
-echo "$(displaySlot "${api}" "${slot}")"
+for releaseName in "${releaseNames[@]}"; do 
+echo "$(displaySlot "${releaseName}" "${slot}")"
 done
 done
