6bae6b681a257732643266685e927bb323ea4815
Dr Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2019-12-12 14:15:35 +0100

    Update fix-reuse-values.sh

--- fix-reuse-values.sh
+++ fix-reuse-values.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+#
+# The ohteamvalues file from the ZIP file
+#
 ohteamvalues="$(cat ./ohteamvalues)"
+
+#
+# The directory where https://github.com/Azure-Samples/openhack-devops-team/ is checked-out to
+#
+sourceDir="."
 
 declare -A charts=(
    ["api-poi"]="apis/poi/charts/mydrive-poi"
@@ -9,14 +17,19 @@ declare -A charts=(
    ["api-user"]="apis/userprofile/charts/mydrive-user"
 )
 
-# for chartName in "${!charts[@]}"; do echo "${chart} lives in ${charts["${chartName}"]}" ; done
+for chartName in "${charts[@]}"; do
+   chartDir="${charts[$chartName]}"
 
-chartName="api-poi"
-chartDir="${charts[$chartName]}"
+   endpoint="$( echo "${ohteamvalues}" | egrep "^endpoint" | awk '{print $2}' )"
+   acr="$( echo "${ohteamvalues}" | egrep "^ACR$(printf '\t')" | awk '{print $2}' )"
+   image="${acr}.azurecr.io/devopsoh/${chartName}"
 
-endpoint="$( echo "${ohteamvalues}" | egrep "^endpoint" | awk '{print $2}' )"
-acr="$( echo "${ohteamvalues}" | egrep "^ACR$(printf '\t')" | awk '{print $2}' )"
-image="${acr}.azurecr.io/devopsoh/${chartName}"
+   echo "Deleting old chart ${chartName}"
+   helm delete --purge "${chartName}"
 
-helm delete --purge "${chartName}"
-helm install "${chartDir}" --name "${chartName}" --set "repository.image=${image},env.webServerBaseUri=http://${endpoint},ingress.rules.endpoint.host=${endpoint}"
+   echo "Installing clean version of ${chartName}"
+   helm install "${chartDir}" --name "${chartName}" \
+       --set "repository.image=${image}" \
+       --set "env.webServerBaseUri=http://${endpoint}" \
+       --set "ingress.rules.endpoint.host=${endpoint}"
+done
